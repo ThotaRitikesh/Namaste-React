11a64f29df0928552d6921116ed15d0344a91a29
ritikesh <thotaritikesh@gmail.com>
2023-05-18 20:12:50 +0530

    epi-02

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-// const heading= React.createElement("h1",{id:"heading",xyz:"abc"},["hello world from react!"]);
-
+import React from "react";
+import ReactDOM from "react-dom/client"
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "nested"),
